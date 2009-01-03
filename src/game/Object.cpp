19df722f3e4db00d14ae83e32b4bdd343f7376ae
Charlie2025 <mivalt.tomas@centrum.cz>
2009-01-03 14:05:46 +0000

    Removed one if statement, caused bug icons

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -568,8 +568,7 @@ void Object::_BuildValuesUpdate(uint8 updatetype, ByteBuffer * data, UpdateMask 
                 IsActivateToQuest = true;
                 updateMask->SetBit(GAMEOBJECT_DYNAMIC);
             }
-            if (GetUInt32Value(GAMEOBJECT_BYTES_1))
-                updateMask->SetBit(GAMEOBJECT_BYTES_1);
+
         }
     }
     else                                                    //case UPDATETYPE_VALUES
