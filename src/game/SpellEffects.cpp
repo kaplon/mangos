89489a0e6004c9b19aca8ed0f4fa1bea27fc93eb
kaplon <kaplonko@gmail.com>
2009-06-29 11:53:07 +0200

    [Fix] Spell 724 and ranks ("Lightwell")

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3211,6 +3211,9 @@ void Spell::EffectSummonType(uint32 i)
         case SUMMON_TYPE_TOTEM:
             EffectSummonTotem(i);
             break;
+        case SUMMON_TYPE_OBJECT:
+       EffectTransmitted(i);
+       break;
         case SUMMON_TYPE_UNKNOWN1:
         case SUMMON_TYPE_UNKNOWN2:
         case SUMMON_TYPE_UNKNOWN3:
