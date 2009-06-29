d4eb40d9fc8ab8aa6ed28c8f759c42fe881e53a6
kaplon <kaplonko@gmail.com>
2009-06-29 12:06:09 +0200

    [Fix] Druid talent "Improved Leader of the Pack"

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5251,6 +5251,19 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
         {
             switch(dummySpell->Id)
             {
+                // Leader of the Pack
+                case 24932:
+                {
+                    if (triggerAmount == 0)
+                        break;
+                    basepoints0 = triggerAmount * GetMaxHealth() / 100;
+                    triggered_spell_id = 34299;
+                    if (triggeredByAura->GetCaster() != this)
+                        break;
+                    int32 basepoints1 = 2*triggerAmount;
+                    CastCustomSpell(this,60889,&basepoints1,0,0,true,0,triggeredByAura);
+                    break;
+                }
                 // Healing Touch (Dreamwalker Raiment set)
                 case 28719:
                 {
@@ -6404,14 +6417,6 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, Aura* triggeredB
                 }
                 //else if (auraSpellInfo->Id==40363)// Entangling Roots ()
                 //    trigger_spell_id = ????;
-                // Leader of the Pack
-                else if (auraSpellInfo->Id == 24932)
-                {
-                    if (triggerAmount == 0)
-                        return false;
-                    basepoints0 = triggerAmount * GetMaxHealth() / 100;
-                    trigger_spell_id = 34299;
-                }
                 break;
             }
             case SPELLFAMILY_HUNTER:
