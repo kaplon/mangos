676b29cf43ce8ebe577ad94dca33c2582956549f
kaplon <kaplonko@gmail.com>
2009-06-30 23:30:47 +0200

    [Fix] Spell 724 and ranks ("Lightwell")

---
+++ sql/updates/custom/patch_lightwell_pasdVn.sql
@@ -0,0 +1,8 @@
+DELETE FROM gameobject_template WHERE entry IN (181102, 181105, 181106, 181165);
+INSERT INTO gameobject_template (entry, type, displayId, name, 	conName, castBarCaption, faction, flags, size, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, scriptName) VALUES
+(31897, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 7001, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(31896, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 27873, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(31895, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 27874, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(31894, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 28276, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(31893, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 48084, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(31883, 22, 6671, 'Lightwell', '', '', 2, 64, 1, 48085, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
