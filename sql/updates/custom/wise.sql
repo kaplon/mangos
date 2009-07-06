10b91d4b2ca265584184fc47f9eb9a3e8b199c4a
kaplon <kaplonko@gmail.com>
2009-07-07 01:04:09 +0200

    Revert "[Fix] Judgement of Wise"
    This reverts commit fca72cdf627384853c1c7d2889f18752a18e8dd2.

--- sql/updates/custom/wise.sql
+++
@@ -1,4 +0,0 @@
-delete from spell_proc_event where entry in (31876,31877,31878);
-insert into spell_proc_event (entry, schoolMask, spellFamilyName, spellFamilyMask0, spellFamilyMask1, spellFamilyMask2, procFlags, procEx, ppmRate, customChance, cooldown) values('31876','0','10','8388608','0','0','16','0','0','33','0');
-insert into spell_proc_event (entry, schoolMask, spellFamilyName, spellFamilyMask0, spellFamilyMask1, spellFamilyMask2, procFlags, procEx, ppmRate, customChance, cooldown) values('31877','0','10','8388608','0','0','16','0','0','66','0');
-insert into spell_proc_event (entry, schoolMask, spellFamilyName, spellFamilyMask0, spellFamilyMask1, spellFamilyMask2, procFlags, procEx, ppmRate, customChance, cooldown) values('31878','0','10','8388608','0','0','16','0','0','100','0');
