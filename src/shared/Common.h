919e20e0a29069f2d55c14b3f2b1000c09245b79
kaplon <kaplonko@gmail.com>
2009-09-10 15:02:59 +0200

    [p005] Ancestors changes gmlevels & server info

--- src/shared/Common.h
+++ src/shared/Common.h
@@ -169,10 +169,12 @@ enum TimeConstants
 enum AccountTypes
 {
     SEC_PLAYER         = 0,
-    SEC_MODERATOR      = 1,
+    SEC_VIP            = 1,
     SEC_GAMEMASTER     = 2,
-    SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+    SEC_BUGTRACKER     = 3,
+    SEC_ADMINISTRATOR  = 4,
+    SEC_SYSOP          = 5,
+    SEC_CONSOLE        = 6                                  // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
