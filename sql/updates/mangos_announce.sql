7dd83f5eb6a7b6097071942f74e5ce0c92f63e8a
kaplon <kaplonko@gmail.com>
2010-01-27 02:03:37 +0100

    fix

--- sql/updates/mangos_announce.sql
+++ sql/updates/mangos_announce.sql
@@ -4,4 +4,4 @@ INSERT INTO mangos_string VALUES
 (65,'|c1f40af20 <GM>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (66,'|c1f40af20 <Admin>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (67,'|c1f40af20 <SysOP>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(68,'|c1f40af20 <CONSOLE>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(68,'|c1f40af20 <CONSOLE>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
